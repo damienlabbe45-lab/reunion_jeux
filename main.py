5ed762486309d07da75f910aa03c0cbd10116ea7
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-16 15:40:14 +0200

    retraits des fonctions utilitaires de l'utilisateur  vers utils_main.py

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-def input_name_user() -> str:
-    """fonction servant à demander à l'utilisateur de mettre un nom"""
-    return input("entrez votre nom")
-
-
-def choice_game_user() -> str:
-    """permet de savoir quel jeu ce sera"""
-    game = None
-    while game is None:
-        game = input("Veuillez indiquer le jeu (nime ou Marienbad ou pmu ou le compte est bon)\n")
-        if game not in ["nime", "Marienbad", "pmu", "le compte est bon"] :
-            game = None
-    return game
-
-
-def input_number_user() -> int:
-    """permet de savoir combien de joueurs il aura"""
-    number = None
-    while number is None:
-        number = input("Veuillez indiquer le nombre de joueurs (1 ou 2)\n")
-        if number in ["1", "2"]:
-            number =int(number)
-        else:
-            number = None
-    return number
-
-
 def game_setup() -> None:
     """prépare les variables ainsi que les configurations si c'est pour jouer contre l'ordi ou pas"""
     from secrets import choice
     from horse import generate_run
     from le_compte_est_bon import game
     from nimes_et_variation import game_nime, game_variation
+    from utils_main import input_name_user, choice_game_user, input_number_user
     game_choice = choice_game_user()
     number_user = input_number_user()
     liste_user = []
