58951890e637f27ec017dd17ba83aa4337841de5
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-16 15:29:47 +0200

    ajout des fonctions utilitaires du jeu de nimes et de sa variation concernant l'ordinateur

--- utils.py
+++ utils.py
@@ -26,3 +26,28 @@ def input_number_matches_user() -> int:
         else:
             number = None
     return number
+
+
+def choice_computer_matches(number_matches: int) -> int:
+    """choix de l'ordinateur pour les allumettes"""
+    if number_matches % 5 != 1 and number_matches > 5:
+        number = 5 - number_matches % 5 
+    elif 1 < number_matches <= 5:
+        number = number_matches - 1
+    else:
+        from secrets import choice
+        number = choice(list(range(1, min(5, number_matches + 1))))
+    return number
+
+
+def choice_computer_heap_matches(list_heap_matches:list[str]) -> int :
+    """fonction permettant à l'ordinateur de choisir le tas d'allumette"""
+    heap_matches = [heap for heap in list_heap_matches if len(heap)> 1]
+    if len(heap_matches) > 1:
+        from secrets import choice
+        heap = list_heap_matches.index(choice(heap_matches))
+    elif len(heap_matches) == 1:
+        heap = list_heap_matches.index(heap_matches[0])
+    else:
+        heap = 0
+    return heap
