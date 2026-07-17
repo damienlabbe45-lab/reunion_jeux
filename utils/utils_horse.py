6fb4b0de786fa48228bb00ea289609e053ddaf36
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-17 16:00:22 +0200

    suppression de la fonction input_number_horse et remplacement des appels de la fonction par une importation de utils_main.py et utilisation de input_number_user à la place

--- utils/utils_horse.py
+++ utils/utils_horse.py
@@ -7,19 +7,6 @@ def print_results(results_run: list[str], type_run: int) -> None:
             print(f" en {i + 1}ème , on a {horse}")
 
 
-def input_number_horse() -> int:  # pyright: ignore[reportReturnType]
-    """cette fonction permet de connaître le nombre de chevaux souhaités par l'utilisateur"""
-    number_horse = None
-    while number_horse is None:
-        input_user = input("Veillez indiquer s'il vous plaît un nombre de chevaux entre 12 et 20")
-        if input_user.isdigit():
-            input_user = int(input_user)
-            if 12 <= input_user <= 20:
-                number_horse = input_user
-    if isinstance(number_horse, int):
-        return number_horse
-
-
 def input_type_run() -> int:  # pyright: ignore[reportReturnType]
     """cette fonction permet de connaître le type de course souhaités par l'utilisateur"""
     type_run = None
@@ -41,7 +28,8 @@ def input_user() -> None:
 def generate_run() -> dict[str, list[int]]:
     """fonction générant un dictionnaire ou la clé est un cheval (horse en aglais signifie cheval) associé 
     à sa distance (la valeur indice 0 en python) et à sa vitesse (valeur indice 1 en python)"""
-    number_horse = input_number_horse()
+    from utils_main import input_number_user
+    number_horse = input_number_user(20,"Veillez indiquer s'il vous plaît un nombre de chevaux entre 12 et 20\n" ,False,12)
     dict_horse = {}
     for horse in range(1, number_horse + 1):
         dict_horse[f"horse {horse}"] = [0, 0]
