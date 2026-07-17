af13b0eeb0f865d2b4c439f0c62d24e2d9e8ca95
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-17 10:38:16 +0200

    suppression de ce qui est doublon par rapport au fichier main.py

--- main_bataille.py
+++ main_bataille.py
@@ -7,19 +7,4 @@ def main():
     matrice: DataFrame = battleship(create_matrice())
     grille_ship = generate_grille(matrice.copy())
     game(grille_ship, matrice)
-    print("merci d'avoir jouer")
-
-
-def user():
-    gaming = None
-    while gaming is None:
-        main()
-        gaming = input("Voulez-vous rejouer?\n")
-        if gaming == "":
-            gaming = None
-        else:
-            print("à une prochaine fois ^^.")
-
-
-if __name__ == "__main__":
-    user()
+    
