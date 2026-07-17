bd2822bd351c78d2803485f8b613c928ab90987c
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-17 10:45:49 +0200

    suppression de l'import inutile fromgame import game dans war_ship

--- game.py
+++ game.py
@@ -3,7 +3,6 @@ from pandas import DataFrame
 
 def war_ship():
     from creation import battleship, generate_grille, create_matrice
-    from game import game
     matrice: DataFrame = battleship(create_matrice())
     grille_ship = generate_grille(matrice.copy())
     game(grille_ship, matrice)
