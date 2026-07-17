ae773f53a83ec953d8613773068c0e9a3ff72222
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-17 15:04:30 +0200

    ajout de l'argument min dans la fonction de input_number_user avec une valeur par défaut et rename de l'argument number_use en max

--- utils_main.py
+++ utils_main.py
@@ -13,12 +13,12 @@ def choice_game_user() -> str:
     return game
 
 
-def input_number_user(number_use: int, prompt: str, boole_ind: bool) -> int:
+def input_number_user(max: int, prompt: str, boole_ind: bool, min: int= 1) -> int:
     """demander à l'utilisateur de dire quel nombre il va utiliser"""
     number = None
     while number is None:
         number = input(prompt)
-        if number in [str(i) for i in range(1, number_use + 1)]:
+        if number in [str(i) for i in range(min, max + 1)]:
             if boole_ind:
                 number =int(number) - 1
             else:
