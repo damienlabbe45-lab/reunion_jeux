16f81428e46caa466d5d3c7d836c2aadf5a2ede8
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-17 11:50:11 +0200

    on met les imports concernant la fonction bulding dans celle-ci

--- main_gratteciel.py
+++ main_gratteciel.py
@@ -1,10 +1,10 @@
 from asyncio import run
-from asyncio import gather
-from creation_gratteciel import generate_solution, combination, create_matrice, suppr_data, intialize
-from game_gratteciel import game
 
 
 async def bulding() -> None:
+    from asyncio import gather
+    from creation_gratteciel import generate_solution, combination, create_matrice, suppr_data, intialize
+    from game_gratteciel import game
     number = intialize()
     matrice = generate_solution(await gather(create_matrice(number), combination(number)))
     matrice_game = suppr_data(matrice.copy(), number)
